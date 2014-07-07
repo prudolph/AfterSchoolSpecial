51572b7b817edcb1105f1cf34ae69d83f2b346bb
Paul Rudolph <Paul.e.rudolph@gmail.com>
2014-07-07 13:02:41 -0400

    Cleaned up paddle class

--- BodyPong/src/Paddle.cpp
+++ BodyPong/src/Paddle.cpp
@@ -24,26 +24,13 @@ Paddle::Paddle()
 
 void Paddle::setup(){
 
-    mPosition=Vec2f(getWindowWidth()/2.0f,getWindowHeight()/2.0f);
-    mDirection = Vec2f(randFloat(0,1),randFloat(0,1));
-    mSpeed = 1.2f;
 }
 void Paddle::update(){
-    if(mPosition.x<0 ||mPosition.x> getWindowWidth() ){
-        mDirection.x*=-1;
-    }
-    if(mPosition.y<0 ||mPosition.y> getWindowHeight() ){
-        mDirection.y*=-1;
-    }
-    
-    mPosition += mDirection*mSpeed;
-
+  
 
 }
 void Paddle::draw(){
     
-    cinder::gl::color(1,1,0);
-    cinder::gl::drawSolidCircle(mPosition, 50.0);
     
     
 }
