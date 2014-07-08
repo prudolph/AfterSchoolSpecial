7b0375b0f3532ecd8109b3db4705742c442bb152
Kathryn Stracquatanio <kathryn@bluecadet.com>
2014-07-08 10:01:25 -0400

    unity body pong files

---
+++ BodyPongUnity/Assets/Scripts/BallController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float cSpeed = 10f;
+	public float force = 20f;
+	public float maxY = 1.5f;
+
+	Vector3 originalPos;
+	Quaternion originalRot;
+	float originalSpeed;
+	
+	// Use this for initialization
+	void Start () {
+
+		originalPos = transform.position;
+		originalRot = transform.rotation;
+		originalSpeed = cSpeed;
+		Reset ();
+	}
+
+	void Reset(){
+		cSpeed = originalSpeed;
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.AddForce(force,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cvel = rigidbody.velocity;
+		Vector3 tvel = cvel.normalized * cSpeed;
+		rigidbody.velocity = Vector3.Lerp(cvel, tvel, Time.deltaTime);
+		cSpeed = cSpeed + .1f;
+		if (transform.position.y > maxY) {
+			transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+		}
+		if (transform.position.y < -4) {
+			transform.position = originalPos;
+			transform.rotation = originalRot;
+			Reset();
+		}
+	}
+
+	void OnCollision(){
+
+	}
+}
---
+++ BodyPongUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5f;
+	public string buttonUp = "UP";
+	public string buttonDown = "DOWN";
+
+	Vector3 originalPos;
+	Quaternion originalRot;
+
+	// Use this for initialization
+	void Start () {
+		originalPos = transform.position;
+		originalRot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton(buttonUp)){
+			transform.Translate(new Vector3(0f, 0 , speed) * Time.deltaTime);
+		}
+		if(Input.GetButton(buttonDown)){
+			transform.Translate(new Vector3(0f, 0 , -speed) * Time.deltaTime);
+		}
+
+		if (transform.position.y < -4) {
+			transform.position = originalPos;
+			transform.rotation = originalRot;
+		}
+	}
+}
